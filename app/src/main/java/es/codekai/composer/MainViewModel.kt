31dbe43819d43dac636a12a7d29b4060d03f2bf2
Juan R. Gavilanes <jrgavilanes@gmail.com>
2022-09-06 01:33:27 +0200

    basic example login

--- app/src/main/java/es/codekai/composer/MainViewModel.kt
+++ app/src/main/java/es/codekai/composer/MainViewModel.kt
@@ -1,14 +1,57 @@
 package es.codekai.composer
 
+import android.util.Patterns
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainViewModel : ViewModel() {
-    private val _email = MutableLiveData<String>()
-    val email: LiveData<String> = _email
 
-    fun onChangeEmail(email: String) {
-        _email.value = email
+    data class LoginUIState(
+        val email: String = "",
+        val password: String = "",
+        val isButtonEnabled: Boolean = false,
+        val isLoading: Boolean = false
+    )
+
+    private val _loginUIState = MutableLiveData<LoginUIState>()
+    val loginUIState: LiveData<LoginUIState> = _loginUIState
+
+    suspend fun onLoginSelected() {
+//    fun onLoginSelected() {
+//        viewModelScope.launch {
+//            val state = _loginUIState.value
+//            _loginUIState.value = state?.copy(isLoading = true, password = "", email = "")
+//            delay(3000)
+//            val state2 = _loginUIState.value
+//            _loginUIState.value = state2?.copy(isLoading = false)
+//        }
+
+        var state = _loginUIState.value
+        _loginUIState.value = state?.copy(isLoading = true, password = "", email = "", isButtonEnabled = false)
+        delay(3000)
+        state = _loginUIState.value
+        _loginUIState.value = state?.copy(isLoading = false)
     }
+
+    fun onDataChange(loginUIState: LoginUIState) {
+        val status = _loginUIState.value
+        status?.let { loginUiState ->
+            val buttonEnabled =
+                isValidEmail(loginUIState.email) && isValidPassword(loginUIState.password)
+
+            _loginUIState.value = loginUiState.copy(
+                email = loginUIState.email,
+                password = loginUIState.password,
+                isButtonEnabled = buttonEnabled
+            )
+        } ?: run { _loginUIState.value = loginUIState }
+    }
+
+    private fun isValidPassword(password: String) = password.length > 1
+
+    private fun isValidEmail(email: String) = Patterns.EMAIL_ADDRESS.matcher(email).matches()
 }
--- app/src/main/java/es/codekai/composer/screens/LoginScreen.kt
+++ app/src/main/java/es/codekai/composer/screens/LoginScreen.kt
@@ -3,28 +3,40 @@ package es.codekai.composer.screens
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import es.codekai.composer.MainViewModel
 import es.codekai.composer.R
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun LoginScreen(viewModel: MainViewModel) {
+    val keyboardController = LocalSoftwareKeyboardController.current
     Box(
         Modifier
             .fillMaxSize()
             .padding(16.dp)
+            .clickable { keyboardController?.hide() }
     ) {
         Login(Modifier.align(Alignment.Center), viewModel)
     }
@@ -32,40 +44,72 @@ fun LoginScreen(viewModel: MainViewModel) {
 
 @Composable
 fun Login(modifier: Modifier, viewModel: MainViewModel) {
-    val email: String by viewModel.email.observeAsState(initial = "")
-    Column(modifier = modifier) {
-        Image(
-            modifier = modifier,
-            painter = painterResource(id = R.drawable.clint2),
-            contentDescription = "header"
-        )
-        Spacer(modifier = Modifier.padding(16.dp))
-        EmailField(email) { viewModel.onChangeEmail(it) }
-        Spacer(modifier = Modifier.padding(4.dp))
-        PasswordField()
-        Spacer(modifier = Modifier.padding(8.dp))
-        Text(
-            text = stringResource(id = R.string.forgot_password),
-            modifier = Modifier
-                .clickable { }
-                .align(Alignment.End),
-            fontSize = 12.sp,
-            fontWeight = FontWeight.Bold,
-            color = Color(0xFF3F51B5)
-        )
-        Spacer(modifier = Modifier.padding(16.dp))
-        LoginButton()
+    val context = LocalContext.current
+    val coroutineScope = rememberCoroutineScope()
+
+    val uiState: MainViewModel.LoginUIState by viewModel.loginUIState.observeAsState(
+        initial = MainViewModel.LoginUIState()
+    )
+
+    if (uiState.isLoading) {
+        Box(Modifier.fillMaxSize()) {
+            CircularProgressIndicator(Modifier.align(Alignment.Center))
+        }
+    } else {
+        Column(modifier = modifier) {
+            Image(
+                modifier = modifier,
+                painter = painterResource(id = R.drawable.clint2),
+                contentDescription = "header"
+            )
+            Spacer(modifier = Modifier.padding(16.dp))
+            EmailField(uiState) { viewModel.onDataChange(it) }
+            Spacer(modifier = Modifier.padding(4.dp))
+            PasswordField(uiState) { viewModel.onDataChange(it) }
+            Spacer(modifier = Modifier.padding(8.dp))
+            Text(
+                text = stringResource(id = R.string.forgot_password),
+                modifier = Modifier
+                    .clickable { }
+                    .align(Alignment.End),
+                fontSize = 12.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color(0xFF3F51B5)
+            )
+            Spacer(modifier = Modifier.padding(16.dp))
+            LoginButton(uiState) {
+                coroutineScope.launch {
+                    viewModel.onLoginSelected()
+                }
+            }
+        }
     }
 }
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
-fun EmailField(email: String, onValueChange: (String) -> Unit) {
+fun EmailField(
+    status: MainViewModel.LoginUIState,
+    onValueChange: (MainViewModel.LoginUIState) -> Unit
+) {
+    val focusRequester = remember { FocusRequester() }
+    val focusManager = LocalFocusManager.current
+    val keyboardController = LocalSoftwareKeyboardController.current
+
     TextField(
-        value = email,
-        onValueChange = { onValueChange(it) },
-        modifier = Modifier.fillMaxWidth(),
+        value = status.email,
+        onValueChange = { onValueChange(status.copy(email = it)) },
+        modifier = Modifier
+            .fillMaxWidth()
+            .focusRequester(focusRequester),
         placeholder = { Text("email") },
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Email,
+            imeAction = ImeAction.Done
+        ),
+        keyboardActions = KeyboardActions(
+            onDone = { focusManager.clearFocus() }
+        ),
         singleLine = true,
         maxLines = 1,
         colors = TextFieldDefaults.textFieldColors(
@@ -73,16 +117,31 @@ fun EmailField(email: String, onValueChange: (String) -> Unit) {
             backgroundColor = Color(0xFFA8C1D5)
         )
     )
+
+//    LaunchedEffect(Unit) {
+//        focusRequester.requestFocus()
+//    }
 }
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
-fun PasswordField() {
+fun PasswordField(
+    uiState: MainViewModel.LoginUIState,
+    onValueChange: (MainViewModel.LoginUIState) -> Unit
+) {
+    val keyboardController = LocalSoftwareKeyboardController.current
     TextField(
-        value = "",
-        onValueChange = {},
+        value = uiState.password,
+        onValueChange = { onValueChange(uiState.copy(password = it)) },
         modifier = Modifier.fillMaxWidth(),
         placeholder = { Text("password") },
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Password,
+            imeAction = ImeAction.Done
+        ),
+        keyboardActions = KeyboardActions(
+            onDone = { keyboardController?.hide() }
+        ),
         singleLine = true,
         maxLines = 1,
         colors = TextFieldDefaults.textFieldColors(
@@ -93,9 +152,9 @@ fun PasswordField() {
 }
 
 @Composable
-fun LoginButton() {
+fun LoginButton(status: MainViewModel.LoginUIState, onLoginSelected: () -> Unit) {
     Button(
-        onClick = { /*TODO*/ },
+        onClick = { onLoginSelected() },
         modifier = Modifier
             .fillMaxWidth()
             .height(48.dp),
@@ -104,7 +163,8 @@ fun LoginButton() {
             disabledBackgroundColor = Color(0xFFFF9800),
             contentColor = Color.White,
             disabledContentColor = Color.White
-        )
+        ),
+        enabled = status.isButtonEnabled
     ) {
         Text("Iniciar sesión")
     }
